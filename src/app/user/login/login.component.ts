9ab3ec9bed9fd38f82152ea9d66c56ae4754bd34
Sakna Hewa Galamulage <sakna.hewagalamulage@eskmapping.com.au>
2022-04-29 11:00:17 +1000

    Section 8: Complete

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  credentials = {email:"", password:""}
+
+  login() {
+    console.log("Called login Form", this.credentials);    
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { LoginComponent } from './login/login.component';
@@ -18,7 +18,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     AuthModalComponent
